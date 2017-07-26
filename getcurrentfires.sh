0dc0d29aee9d4ba30637cc2d2ea63c43f167a811
Chris Waigl <chris.waigl@gmail.com>
2017-07-25 19:04:22 -0800

    Don't overwrite existing.

--- getcurrentfires.sh
+++ getcurrentfires.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
-rsync -rldvz --dry-run cwaigl@chinook.alaska.edu:/center1/d/UAFSMOKE/src/firemap/archive/*blmfiredata.txt archive/
+rsync -rldvz --ignore-existing --dry-run cwaigl@chinook.alaska.edu:/center1/d/UAFSMOKE/src/firemap/archive/*blmfiredata.txt archive/
 
-read -p "Continue uploading files (y/N)? " confirm
+read -p "Continue downloading files (y/N)? " confirm
 if [ "$confirm" != "y" ]; then
     echo "Aborting"
     exit 1
 fi
 
-rsync -rldvz cwaigl@chinook.alaska.edu:/center1/d/UAFSMOKE/src/firemap/archive/*blmfiredata.txt archive/
+rsync -rldvz --ignore-existing cwaigl@chinook.alaska.edu:/center1/d/UAFSMOKE/src/firemap/archive/*blmfiredata.txt archive/
